d855ae0970995d2950ba2bf77a7ea1743d9b4463
Kevin Retzke <retzkek@gmail.com>
2020-11-11 20:23:21 -0600

    try tui instead of cursive

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,14 +1,37 @@
 use std::io;
-use cursive::views::{TextView,TextArea,LinearLayout};
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use termion::raw::IntoRawMode;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+
 use super::Program;
 
 pub fn debug(prog: &mut Program) -> io::Result<()> {
-    let mut siv = cursive::default();
-    siv.add_global_callback('q', |s| s.quit());
-    //siv.add_layer(TextView::new("Hello cursive! Press <q> to quit."));
-    siv.add_fullscreen_layer(LinearLayout::vertical()
-                  .child(TextArea::new().content("source here"))
-                  .child(TextView::new("info here")));
-    siv.run();
-    Ok(())
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(50),
+                    Constraint::Percentage(40),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        let block = Block::default()
+             .title("Code")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+
+        let block = Block::default()
+             .title("Block 2")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })
 }
